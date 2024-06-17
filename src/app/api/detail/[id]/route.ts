556f15db585e27edd6b8faeb21f6635055e6b53a
esau22 <emr.emr.espiritu@gmail.com>
2024-06-16 22:26:27 -0500

    cambio el ts

--- src/app/api/detail/[id]/route.ts
+++ src/app/api/detail/[id]/route.ts
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export async function GET(request: any, { params }) {
+interface Params {
+  id: string;
+}
+
+export async function GET(request: Request, { params }: { params: Params }) {
   const id = parseInt(params.id);
   try {
     const users = await prisma.documento.findUnique({
@@ -13,6 +17,9 @@ export async function GET(request: any, { params }) {
     return NextResponse.json(users);
   } catch (error) {
     console.error("Error fetching users:", error);
-    return NextResponse.error("Internal Server Error", 500);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
